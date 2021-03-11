f003de899b2512fdca9c8acab1c831cd55632681
felipeDesenvolvedor <felipedasilvasantos30@gmail.com>
2021-03-11 17:40:18 -0300

    Listagem de usuarios cadastrados

--- build/js/index.js
+++ build/js/index.js
@@ -1,5 +1,63 @@
-import user from './user.js';
+import User from './classes/User';
+import {checkPage} from './utils';
+
+const actionEdit  = () => {
+    
+}
+
+const listUser = () => {
+    const user = new User();
+    const tbody  = document.querySelector('.table__body');
+    let users;
+
+    
+    users = user.listAll();
+    users = users.map(element => {
+        let tr            = document.createElement('tr')
+        // colounas
+        let tdNome        = document.createElement('td')
+        let tdEmail       = document.createElement('td')
+        let tdCPF         = document.createElement('td')
+        let tdTelefone    = document.createElement('td')
+        //textos
+        tdNome.textContent = element.nomeCompleto     
+        tdEmail.textContent = element.email
+        tdCPF.textContent = element.cpf
+        tdTelefone.textContent = element.telefone
+        //buttons
+        let tdEditar      = document.createElement('td')
+        let tdExcluir     = document.createElement('td')
+        let buttonEditar  = document.createElement('button')
+        let buttonExcluir = document.createElement('button')
+
+            buttonEditar.textContent = "Editar";
+            buttonExcluir.textContent = "Excluir";
+
+
+        tdEditar.appendChild(buttonEditar);
+        tdExcluir.appendChild(buttonExcluir);
+
+
+        tr.appendChild(tdNome);
+        tr.appendChild(tdEmail);
+        tr.appendChild(tdCPF);
+        tr.appendChild(tdTelefone);
+        tr.appendChild(tdEditar);
+        tr.appendChild(tdExcluir);
+
+        return tr;
+    });
+
+    users.forEach(element => {
+        tbody.appendChild(element);
+    })
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     
+    if(!checkPage('pageIndex')) {
+        return;
+    }
+
+    listUser();
 });
